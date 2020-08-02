2e17a25629388284018ff3ecb3626a2e3193d007
Praj-akta <prajakta.limje@infojiniconsulting.com>
2020-08-02 17:25:31 +0530

    made changes in banner css

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Home from '../src/screens/Home';
+import About from '../src/screens/AboutUs';
 import { Route, Switch } from 'react-router-dom';
-// import Preview from '../src/screens/Preview';
-// import Dashboard from '../src/screens/Dashboard';
 
 export default class Main extends Component {
     render() {
@@ -11,15 +10,9 @@ export default class Main extends Component {
                 <Route path = "/" render = {props => {
                     return <Home {...props}/>
                 }} exact />
-                {/* <Route path = "/menus/:menuId/sub-menu/:subMenuId/children/:childrenId/:viewOption" render = {props => {
-                    return <Dashboard {...props}/>
-                }} />
-                <Route path = "/menus/:menuId/sub-menu/:subMenuId/:viewOption" render = {props => {
-                    return <Dashboard {...props}/>
-                }} />
-                <Route path = "/preview/:previewId/:viewOption" render = {props => {
-                    return <Preview {...props}/>
-                }} /> */}
+                <Route path = "/about" render = {props => {
+                    return <About {...props}/>
+                }} exact />
             </Switch>      
         );
     }
